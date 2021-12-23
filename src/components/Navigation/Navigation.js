dbf97dd87325b1e6ed850be99da033d76176a3b5
Eliott Morcillo <eliott.morcillo@gmail.com>
2021-12-23 11:42:27 +0100

    feat: mobile nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,25 +3,51 @@ import { Link } from "react-router-dom";
 import HamburgerIcon from "./HambugerIcon/HamburgerIcon";
 import "./Navigation.css";
 const Navigation = ({ links, logo, size }) => {
-  const [open, setOpen] = React.useState(size >= 700 ? true : false);
+  const [open, setOpen] = React.useState(false);
+  React.useEffect(() => {
+    if (size.x >= 700) setOpen(false);
+  }, [size.x, setOpen]);
 
-  return (
-    <nav className="main_navigation">
-      <Link to="/">
-        <img src={logo} alt="logo" className="logo_navigation" />
-      </Link>
-      <ul className="links_list">
+  const MobileNavigation = () => (
+    <div
+      className={
+        open
+          ? "mobile_navigation_container"
+          : "mobile_navigation_container hidden"
+      }
+    >
+      <ul className="mobile_links_list">
         {links.map((link) => (
           <li key={link.label}>
             <Link to={link.path}>{link.label}</Link>
           </li>
         ))}
       </ul>
-      <button className="login_button" id="nav_login">
-        Login
-      </button>
-      <HamburgerIcon open={open} setOpen={setOpen} />
-    </nav>
+      <button className="login_button">
+          Login
+        </button>
+    </div>
+  );
+  return (
+    <>
+      <nav className="main_navigation">
+        <Link to="/">
+          <img src={logo} alt="logo" className="logo_navigation" />
+        </Link>
+        <ul className="links_list">
+          {links.map((link) => (
+            <li key={link.label}>
+              <Link to={link.path}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+        <button className="login_button" id="nav_login">
+          Login
+        </button>
+        <HamburgerIcon open={open} setOpen={setOpen} />
+      </nav>
+      <MobileNavigation />
+    </>
   );
 };
 
